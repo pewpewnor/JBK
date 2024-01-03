80225eecd2d7fea998b6bb570b5b4979e42200c5
pewpewnor <newpatchnotworking2@tutanota.com>
2024-01-03 19:31:54 +0700

    check login

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
     //
     public function index() {
+        if (!Auth::check()) {
+            return redirect('/LoginUser');
+        }
         return view('Category', ["categories" => Category::with('data')->get()]);
     }
 }
